4937c7ae6660aed369cf53624fb71fbb7bdefcbd
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2020-08-22 18:57:10 +0200

    Initialize project using Create React App

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAlCZfq6cpkAcaljfkQk-oIMtQkjLfa9e0",
+    authDomain: "whats-app-4add4.firebaseapp.com",
+    databaseURL: "https://whats-app-4add4.firebaseio.com",
+    projectId: "whats-app-4add4",
+    storageBucket: "whats-app-4add4.appspot.com",
+    messagingSenderId: "182714807535",
+    appId: "1:182714807535:web:ea94dd16573e4750a4571a",
+    measurementId: "G-R1TW0HCH96"
+};
